4de4a766de19200b9bd1ddccbf7c3ce8984e095c
narpalan <thiagofumega@gmail.com>
2026-02-05 19:37:33 -0300

    chore: add import type statement.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Item, ItemFormData } from '../types/item';
+import type { Item, ItemFormData } from '../types/item';
 
 const api = axios.create({
     baseURL: 'http://localhost:5000',
